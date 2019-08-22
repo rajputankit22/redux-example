c21297770d2fda27404f4a8f9996e513e80adfa7
rajputankit22 <rajputankit22@gmail.com>
2019-08-22 18:04:58 +0530

    install redux

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React from "react";
 import "./App.css";
 import Post from "./components/Posts";
 import PostForm from "./components/Postform";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Post />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <hr />
+        <Post />
+      </div>
+    </Provider>
   );
 }
 
